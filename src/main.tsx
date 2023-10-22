741fdbb63af253a943841b18973b745c19e2970f
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2023-10-22 01:02:16 -0300

    created routers fixed import

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./utils/queryClient.ts";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routers/routers.ts"
+import { router } from "./routers/routers.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
